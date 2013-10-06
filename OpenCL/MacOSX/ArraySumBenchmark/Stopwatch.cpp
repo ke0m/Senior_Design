7eb3dbe6c5f25427723f2afb8b277427c2044c92
Spencer Haich <shaich@mines.edu>
2013-10-05 18:14:20 -0600

    Updated Stopwatch to measure time more accurately

--- OpenCL/MacOSX/ArraySumBenchmark/Stopwatch.cpp
+++ OpenCL/MacOSX/ArraySumBenchmark/Stopwatch.cpp
@@ -1,6 +1,5 @@
 #include "Stopwatch.h"
 #include <iostream>
-#include <time.h>
 
 /**
  * A timer that works like a common stopwatch
@@ -8,20 +7,25 @@
  * @version 2004.11.02
  * translated into C++ by Spencer Haich, Colorado School of Mines
  */
+
+Stopwatch::Stopwatch(){
+    _running=false;
+    _start=0;
+    _time=0;
+}
+
 void Stopwatch::start() {
     if (!_running) {
-      time_t timer;
-      timer = time(NULL);
+      gettimeofday(&begtime,NULL);
       _running = true;
-      _start = timer;
+      _start = begtime.tv_sec + begtime.tv_usec/1.0e6;
     }
 }
 
 void Stopwatch::stop() {
     if (_running) {
-      time_t timer;
-      timer = time(NULL);
-      _time += timer-_start;
+      gettimeofday(&endtime,NULL);
+      _time += endtime.tv_sec + endtime.tv_usec/1.0e6 - _start;
       _running = false;
     }
 }
@@ -35,14 +39,11 @@ void Stopwatch::restart() {
     reset();
     start();
 }
-
+  
 double Stopwatch::getTime() {
     if (_running) {
-      time_t timer;
-      timer = time(NULL);
-      return _time+(timer-_start);
+      gettimeofday(&nowtime,NULL);
+      return nowtime.tv_sec + nowtime.tv_usec/1.0e6 - _start;
     }
     return _time;
 }
-  
-
--- OpenCL/MacOSX/ArraySumBenchmark/Stopwatch.h
+++ OpenCL/MacOSX/ArraySumBenchmark/Stopwatch.h
@@ -1,15 +1,17 @@
 #pragma once
+#include <sys/time.h>
 
 class Stopwatch {
-    private:
-      bool _running;
-      long _start;
-      double _time;
-    public:
-      Stopwatch() { _running=false; _start=0; _time=0;};
-      void start();
-      void stop();
-      void reset();
-      void restart();
-      double getTime();
+  private:
+    struct timeval begtime, nowtime, endtime;
+    bool _running;
+    long _start;
+    double _time;
+  public:
+    Stopwatch();
+    void start();
+    void stop();
+    void reset();
+    void restart();
+    double getTime();
 };
