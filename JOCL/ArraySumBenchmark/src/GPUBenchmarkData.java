b2beab4e349aee76407d5d1c2dccf55cb317a574
Joseph Jennings <jojennin@mines.edu>
2013-11-22 19:58:31 -0700

    Added new GPU leveled Plot

--- JOCL/ArraySumBenchmark/src/GPUBenchmarkData.java
+++ JOCL/ArraySumBenchmark/src/GPUBenchmarkData.java
@@ -8,29 +8,34 @@ public class GPUBenchmarkData {
 	
 	public static void main(String args[]) throws FileNotFoundException{
 		
-		//BenchmarkGPULoop data = new BenchmarkGPULoop();
+		BenchmarkGPULoop data = new BenchmarkGPULoop();
 		//BenchmarkCPULoop datacpu = new BenchmarkCPULoop();
-		BenchmarkOCLCPU dataoclcpu = new BenchmarkOCLCPU();
+		//BenchmarkOCLCPU dataoclcpu = new BenchmarkOCLCPU();
 		
 		int minArraySize = 500;
 		int maxArraySize = 10000;
-		int minIters = 0;
-		int maxIters = 1000;
+		int minIters = 1;
+		int maxIters = 100000;
 		int k=0;
 		int z=0;
-		float[][] flopValues = new float[maxArraySize][maxIters];
+		int sizeStep = 100;
+		int iterStep = 10;
+		int row = (maxArraySize - minArraySize)/sizeStep;
+		float[][] flopValues = new float[row][8];
 		PrintStream output = new PrintStream(new File("data.txt"));
 		
 		for(int i = minArraySize; i <= maxArraySize; i+=100){
 			k++;
+			z = 0;
 			System.out.println(i);
 			output.println();
-			for(int j = minIters; j<=maxIters; j+=100){
-				//flopValues[k][z] = data.runBenchmark(j, i, i+1);
+			for(int j = minIters; j<=maxIters; j*=10){
+				flopValues[k][z] = data.runBenchmark(j, i, i+1);
 				//flopValues[k][z] = datacpu.runBenchmarkCPU(j, i, i+1);
-				flopValues[k][z] = dataoclcpu.runBenchmarkOCLCPU(j, i, i+1);
+				//flopValues[k][z] = dataoclcpu.runBenchmarkOCLCPU(j, i, i+1);
 				output.print(flopValues[k][z] + " ");
 				z++;
+				System.out.println(z);
 				
 			}
 		}		
