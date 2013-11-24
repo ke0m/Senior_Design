0c55999dc7ae3f4d38f49e459d76b509bd6afa7f
Joseph Jennings <jojennin@mines.edu>
2013-11-23 20:31:38 -0700

    Added the ArraySumBenchmark for Linux

---
+++ OpenCL/Linux/ArraySumBenchmark/BenchmarkGPU.h
@@ -0,0 +1,23 @@
+//
+//  BenchmarkGPU.h
+//  ArraySumBenchmark
+//
+//  Created by Joseph Jennings on 11/23/13.
+//  Copyright (c) 2013 Joseph Jennings. All rights reserved.
+//
+
+#ifndef __ArraySumBenchmark__BenchmarkGPU__
+#define __ArraySumBenchmark__BenchmarkGPU__
+
+#include <iostream>
+
+class BenchmarkGPU{
+    
+    
+public:
+    
+    float runGPUBenchmark(int iters, int n1, int n2);
+};
+
+
+#endif /* defined(__ArraySumBenchmark__BenchmarkGPU__) */
