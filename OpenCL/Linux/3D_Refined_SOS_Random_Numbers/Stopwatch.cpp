46b81d3a1b3d98dcbe2d1aa8c693b7f9400be1d0
ke0m <joseph.s.jennings@gmail.com>
2014-12-29 21:26:09 -0700

    Stopwatch class for performance measurements

---
+++ OpenCL/Linux/3D_Refined_SOS_Random_Numbers/Stopwatch.cpp
@@ -0,0 +1,54 @@
+#include "Stopwatch.h"
+#include <iostream>
+#include <time.h>
+
+/**
+ * A timer that works like a common stopwatch
+ * @author Dave Hale, Colorado School of Mines
+ * @version 2004.11.02
+ * translated into C++ by Spencer Haich, Colorado School of Mines
+ */
+
+/* Constructor and member intialization list */
+Stopwatch::Stopwatch() : _running(false), _start(0), _time(0)
+{
+}
+
+void Stopwatch::start() {
+    if (!_running) {
+      time_t timer;
+      timer = time(NULL);
+      _running = true;
+      _start = timer;
+    }
+}
+
+void Stopwatch::stop() {
+    if (_running) {
+      time_t timer;
+      timer = time(NULL);
+      _time += timer-_start;
+      _running = false;
+    }
+}
+ 
+void Stopwatch::reset() {
+    stop();
+    _time=0; 
+}
+
+void Stopwatch::restart() {
+    reset();
+    start();
+}
+
+long Stopwatch::getTime() {
+    if (_running) {
+      time_t timer;
+      timer = time(NULL);
+      return _time+(timer-_start);
+    }
+    return _time;
+}
+  
+
---
+++ OpenCL/Linux/3D_Refined_SOS_Random_Numbers/Stopwatch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Stopwatch {
+    private:
+      bool _running;
+      long _start;
+      long _time;
+    public:
+	  //Stopwatch() { _running=false; _start=0; _time=0;};
+	  Stopwatch();
+      void start();
+      void stop();
+      void reset();
+      void restart();
+      long getTime();
+};
